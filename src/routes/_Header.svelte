c50773832f8db6379a4948a69f18085535936212
Robert Lynch <robertlynch2012@gmail.com>
2021-09-05 21:16:54 -0500

    start adding basic page layout with tailwind

---
+++ src/routes/_Header.svelte
@@ -0,0 +1,39 @@
+<script lang="ts">
+  import { assets } from '$app/paths';
+  import { page } from '$app/stores';
+
+  $: path = $page.path;
+</script>
+
+<header>
+  <div class="flex justify-between">
+    <div class="text-xl flex leading-9">
+      <img
+        src={`${assets}/assignment.png`}
+        alt="Assignment Icon"
+        class="w-10 h-10 mr-3"
+      />
+      <h1 class="font-semibold">DRG Completionist</h1>
+    </div>
+    <h2 class="text-gray-400 font-medium leading-10">
+      Leave No Rock or Stone Unturned!
+    </h2>
+  </div>
+  <nav class="flex space-x-6 mt-5">
+    <a href="/" class:active={path === '/'}>Overview</a>
+    <a href="overclocks" class:active={path === '/overclocks'}> Overclocks </a>
+    <a href="frameworks" class:active={path === '/frameworks'}> Frameworks </a>
+    <a href="armor" class:active={path === '/armor'}>Armor</a>
+    <a href="pickaxes" class:active={path === '/pickaxes'}> Pickaxes </a>
+  </nav>
+</header>
+
+<style lang="postcss">
+  .active {
+    @apply border-blue-500 text-blue-400 border-b-2;
+  }
+
+  nav > a {
+    @apply py-2;
+  }
+</style>
---
+++ src/routes/__layout.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  import '../app.postcss';
+  import Header from './_Header.svelte';
+</script>
+
+<div class="flex justify-center">
+  <div class="w-3/4 px-6 pt-6">
+    <Header />
+    <main>
+      <slot />
+    </main>
+    <footer />
+  </div>
+</div>

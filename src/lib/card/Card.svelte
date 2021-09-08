a5342ce333a66a73e3db6f96fcfcd93b2fcbefe0
Robert Lynch <robertlynch2012@gmail.com>
2021-09-07 19:44:20 -0500

    add basic layout for item cards

--- src/lib/card/Card.svelte
+++ src/lib/card/Card.svelte
@@ -11,8 +11,27 @@
 <section class="border border-b-0 last:border-b border-gray-500">
   <CardHeader {expanded} on:click={toggleExpand} />
   {#if expanded}
-    <div class="p-6 border-gray-500 border-t" transition:slide|local>
-      <h4>Card Content</h4>
+    <div
+      class="p-2 border-gray-500 border-t grid grid-cols-3"
+      transition:slide|local
+    >
+      <div class="m-2 border border-gray-500 transition-all hover:shadow-xl">
+        <div class="p-2 bg-yellow-300 text-black font-bold">Lighter Tanks</div>
+        <div>Content!</div>
+      </div>
+      <div
+        class="m-2 border border-gray-500 transition-all hover:shadow-xl"
+        role="button"
+      >
+        <div class="p-2 bg-yellow-300 text-black font-bold">
+          Sticky Additave
+        </div>
+        <div>Content!</div>
+      </div>
+      <div class="m-2 border border-gray-500 transition-all hover:shadow-xl">
+        <div class="p-2 bg-yellow-300 text-black font-bold">Lighter Tanks</div>
+        <div>Content!</div>
+      </div>
     </div>
   {/if}
 </section>

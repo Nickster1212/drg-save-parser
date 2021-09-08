e8f29d261458aa8babb746748f70d243b64d567c
Robert Lynch <robertlynch2012@gmail.com>
2021-09-07 20:38:46 -0500

    move section content into card component

---
+++ src/lib/card/Card.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  export let title: string;
+</script>
+
+<div
+  class="m-2 border border-gray-500 transition-all hover:shadow-xl"
+  role="button"
+  on:click
+>
+  <div class="p-2 bg-yellow-300 text-black font-bold">{title}</div>
+  <div>Content!</div>
+</div>
--- src/lib/section/Section.svelte
+++ src/lib/section/Section.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
   import { slide } from 'svelte/transition';
   import SectionHeader from './SectionHeader.svelte';
+  import Card from '$lib/card/Card.svelte';
 
   let expanded = true;
   function toggleExpand() {
@@ -15,23 +16,12 @@
       class="p-2 border-gray-500 border-t grid grid-cols-3"
       transition:slide|local
     >
-      <div class="m-2 border border-gray-500 transition-all hover:shadow-xl">
-        <div class="p-2 bg-yellow-300 text-black font-bold">Lighter Tanks</div>
-        <div>Content!</div>
-      </div>
-      <div
-        class="m-2 border border-gray-500 transition-all hover:shadow-xl"
-        role="button"
-      >
-        <div class="p-2 bg-yellow-300 text-black font-bold">
-          Sticky Additave
-        </div>
-        <div>Content!</div>
-      </div>
-      <div class="m-2 border border-gray-500 transition-all hover:shadow-xl">
-        <div class="p-2 bg-yellow-300 text-black font-bold">Lighter Tanks</div>
-        <div>Content!</div>
-      </div>
+      <Card title="Lighter Tank" />
+      <Card title="Sticky Additive" />
+      <Card title="Compact Feed Valves" />
+      <Card title="Lighter Tank" />
+      <Card title="Sticky Additive" />
+      <Card title="Compact Feed Valves" />
     </div>
   {/if}
 </section>

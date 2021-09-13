e2f3e372aca6995e97321be5f1b2f20ecb07f416
Robert Lynch <robertlynch2012@gmail.com>
2021-09-12 20:18:23 -0500

    move asset files to lib

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,1 +1,13 @@
 /// <reference types="@sveltejs/kit" />
+
+type PNGSrc = {
+  png: string;
+  webp: string;
+};
+
+type JPGSrc = {
+  jpg: string;
+  webp: string;
+};
+
+declare type ImgSrc = PNGSrc | JPGSrc;

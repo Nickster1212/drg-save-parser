08e13551ef299d9df4dded82acfaa399d0390084
Robert Lynch <robertlynch2012@gmail.com>
2021-10-02 16:30:42 -0500

    abstract out weapon divider to its own component and add tooltip

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,11 +1,12 @@
 <script lang="ts">
   import Section from '$lib/section/Section.svelte';
   import { Miner } from '$lib/types/miner';
-  import { MinerWeapons, WeaponOutlines } from '$lib/types/weapons';
+  import { MinerWeapons } from '$lib/types/weapons';
   import { Overclocks } from '$lib/types/overclocks';
   import { assets } from '$app/paths';
   import Card from '$lib/card/Card.svelte';
   import SectionHeaderProgress from '$lib/section/SectionHeaderProgress.svelte';
+  import WeaponDivider from './_WeaponDivider.svelte';
 
   function enumKeys<O extends object, K extends keyof O = keyof O>(
     obj: O
@@ -31,13 +32,7 @@
     </svelte:fragment>
     <svelte:fragment>
       {#each MinerWeapons[miner] as weapon}
-        <div class="weapon-icon-divider col-span-full p-2 w-full flex">
-          <img
-            src={WeaponOutlines[weapon].webp}
-            alt={weapon}
-            class="px-4 h-[75px]"
-          />
-        </div>
+        <WeaponDivider {weapon} />
         {#each Overclocks[weapon] as overclock}
           <Card title={overclock.name} />
         {/each}
@@ -45,11 +40,3 @@
     </svelte:fragment>
   </Section>
 {/each}
-
-<style lang="postcss">
-  .weapon-icon-divider::before,
-  .weapon-icon-divider::after {
-    @apply border-solid border-t border-t-gray-600 transform translate-y-1/2 w-1/2;
-    content: '';
-  }
-</style>

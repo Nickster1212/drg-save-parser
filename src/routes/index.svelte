72f4bf02090bb858db9399697868b5ad94c84157
Robert Lynch <robertlynch2012@gmail.com>
2021-10-02 13:23:18 -0500

    flatten overclock list and style the weapon divider better

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -31,13 +31,25 @@
     </svelte:fragment>
     <svelte:fragment>
       {#each MinerWeapons[miner] as weapon}
-        <div class="col-span-full">
-          <img src={WeaponOutlines[weapon].webp} alt={weapon} />
+        <div class="weapon-icon-divider col-span-full p-2 w-full flex">
+          <img
+            src={WeaponOutlines[weapon].webp}
+            alt={weapon}
+            class="px-4 h-[75px]"
+          />
         </div>
-        {#each Overclocks[miner][weapon] as overclock}
+        {#each Overclocks[weapon] as overclock}
           <Card title={overclock.name} />
         {/each}
       {/each}
     </svelte:fragment>
   </Section>
 {/each}
+
+<style lang="postcss">
+  .weapon-icon-divider::before,
+  .weapon-icon-divider::after {
+    @apply border-solid border-t border-t-gray-600 transform translate-y-1/2 w-1/2;
+    content: '';
+  }
+</style>

a6b571f747ba2f47ae4e93944811a0c1386db305
Robert Lynch <robertlynch2012@gmail.com>
2022-01-28 22:52:26 -0600

    Start overview layout

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,9 +1,23 @@
 <script lang="ts">
+  import { overclocks } from '$lib/stores/overclocks';
+  import { Overclocks } from '$lib/types/overclocks';
 </script>
 
 <section class="border border-b-0 last:border-b border-gray-500">
   <h2>Overclocks</h2>
-  <h2>Overclocks</h2>
-  <h2>Overclocks</h2>
-  <h2>Overclocks</h2>
+  {#if $overclocks.loading === false}
+    <p>
+      {$overclocks.overclocks.length} / {Object.values(Overclocks).flat()
+        .length}
+    </p>
+  {/if}
+  <h2>Frameworks</h2>
+  <h2>Armor</h2>
+  <h2>Pickaxes</h2>
 </section>
+
+<style lang="postcss">
+  h2 {
+    @apply text-xl;
+  }
+</style>
